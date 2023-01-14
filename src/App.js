4e6bd37f9a62ee273302f18bd40d5b4304bafa7f
batuhanOnur <49373188+batuhanOnur@users.noreply.github.com>
2023-01-14 13:08:01 +0300

    components imported

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import UserForm from "./UserForm";
+import UserList from './UserList';
+
 
 function App() {
+
+  const [users,setUsers] = useState([])
+
+
+  const onUserAdd = (user) => {
+    setUsers([...users,user])
+  }
+
   return (
     <div className="App">
-      <UserForm />
+      <UserForm onUserAdd={onUserAdd}/>
+      <hr/>
+
+      <UserList users={users} />
     </div>
   );
 }
