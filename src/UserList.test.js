09d67729ac5961f79d479c64850c2d17846782f9
batuhanOnur <49373188+batuhanOnur@users.noreply.github.com>
2023-01-14 14:59:56 +0300

    withtestid

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { render, screen,within } from '@testing-library/react'
 import UserList from './UserList';
 
 
@@ -15,10 +15,10 @@ test('render the correct number of rows', () => {
     // * find all the rows in the table
     // screen.logTestingPlaygroundURL();
 
-    // const rows = within(screen.getByTestId('users')).getAllByRole('row')
+    const rows = within(screen.getByTestId('users')).getAllByRole('row')
 
     // eslint-disable-next-line
-    const rows = container.querySelectorAll('tbody tr')
+    //const rows = container.querySelectorAll('tbody tr')
 
 
     // * assertion
